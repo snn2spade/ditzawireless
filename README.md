a4d4586fb292ee85338798def2a0bfb3403f2478
nitipot <nitipot@localhost.localdomain>
2016-11-09 13:57:30 +0700

    car and station +adhoc connection

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ditzawireless
+###To start adhoc
+view your wireless interface and change it in adhoc.sh and run:
+'''
+sudo bash adhoc.sh 
+'''
+
---
+++ car-receive.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import socket
+
+import socket
+address = ('', 10101)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
+server_socket.bind(address)
+serial="AsdfDSfe3$"
+brand="toyota"
+color="red"
+addr_old=""
+while True:
+    recv_data, addr = server_socket.recvfrom(2048)
+    if(recv_data=='hi' and not addr_old==addr):
+	print 'data received'
+        server_socket.sendto(str((serial,brand,color)), addr)
+        addr_old=addr
+        print addr,':',recv_data
+        #server_socket.sendto("*"+recv_data, addr)
